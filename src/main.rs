fc5104c635a0dcb8acaddb211ab2a40ef48dcbc4
Wong Jia Hau <hou32hou@gmail.com>
2021-12-03 16:33:39 +0800

    fix(clock): hand is able to touch the circumference of the clock now

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ fn run_clock() -> Result<(), String> {
             height,
             midpoint_x,
             midpoint_y,
-            circle_radius: midpoint_x.min(midpoint_y) / 1.75,
+            circle_radius: midpoint_x.min(midpoint_y) / 1.1,
+            aspect_ratio: 4.0,
         };
         let datetime: DateTime<Local> = Local::now();
 
@@ -36,30 +37,40 @@ fn run_clock() -> Result<(), String> {
         let degree_minute = (minute + second / 60.0) / 60.0 * 360.0;
         let degree_hour = (hour + minute / 60.0) / 12.0 * 360.0;
 
-        let matrix = draw_line(
+        let matrix = draw_hand(
             matrix,
-            Line {
+            Hand {
                 degree: degree_second,
-                width: 1.0,
-                length: 1.0,
-            },
-        );
-        let matrix = draw_line(
-            matrix,
-            Line {
-                degree: degree_minute,
                 width: 2.0,
-                length: 1.0,
+                length: 0.9,
             },
         );
-        let matrix = draw_line(
+        let matrix = draw_hand(
             matrix,
-            Line {
-                degree: degree_hour,
+            Hand {
+                degree: degree_minute,
                 width: 3.0,
-                length: 0.7,
+                length: 0.9,
             },
         );
+        let matrix = draw_hand(
+            matrix,
+            Hand {
+                degree: degree_hour,
+                width: 4.0,
+                length: 0.6,
+            },
+        );
+
+        // Draw clock face
+        // let matrix = (0..12).into_iter().fold(matrix, |matrix, n|{
+        //     draw_hand(matrix, Line {
+        //         degree: (n as f32) / 12.0 * 360.0,
+        //         width: 2.0,
+        //         length: 1.0
+        //     })
+
+        // });
         print_matrix(matrix);
         std::thread::sleep(Duration::from_millis(100));
 
@@ -79,30 +90,26 @@ struct Matrix {
     midpoint_x: f32,
     midpoint_y: f32,
     circle_radius: f32,
+
+    /// The height of character divides by the width of character in the terminal.
+    /// Cannot be obtained programmatically, thus must be measured manually by user.
+    /// Aspect ratio is necesary to make the circle look rounder, otherwise it will looks like a vertical ellipse.
+    aspect_ratio: f32,
 }
 
 fn draw_circle(matrix: Matrix) -> Matrix {
     let midpoint_x = matrix.midpoint_x;
     let midpoint_y = matrix.midpoint_y;
     let radius = matrix.circle_radius;
-    let border_width = 1.0;
-    let delta_upper = (radius as f32 + border_width).powf(2.0);
-    let delta_lower = (radius as f32 - border_width).powf(2.0);
+    let aspect_ratio = matrix.aspect_ratio;
 
     // Paint circle based on approx. of the circle equation, (x - midpoint_x)^2 + (y - midpoint_y)^2 = radius^2
-
-    // The height of character divides by the width of character in the terminal
-    // Cannot be obtained programmatically, thus must be measured manually by user
-    // Aspect ratio is necesary to make the circle look rounder, otherwise it will looks like
-    // a vertical ellipse
-    let aspect_ratio = 4_f32;
-
     draw_using_equation(matrix, |x, y| {
         let left = ((x as f32) - midpoint_x).powf(2.0) / aspect_ratio
             + ((y as f32) - midpoint_y).powf(2.0);
         let right = radius.powf(2.0);
         let diff = left - right;
-        delta_lower <= diff && diff <= delta_upper
+        diff.abs() < radius
     })
 }
 
@@ -126,51 +133,56 @@ where
     matrix
 }
 
-struct Line {
+struct Hand {
+    /// 0 to 360, where:
+    /// 0 = North,
+    /// 90 = East,
+    /// 180 = South,
+    /// 270 = West.
     degree: f32,
     width: f32,
     /// In terms of percentage. 0 is shortest, 1 is longest.
     length: f32,
 }
 
-/// Draw a line originated from the center,
-/// with the given degree (where 0 means north).
-/// The formula use here is: `(x - midpoint_x) = (tan s) * (y + midpoint_y)`
-/// where `s` is in terms of radian.
-fn draw_line(matrix: Matrix, line: Line) -> Matrix {
-    let degree = line.degree;
+/// Draw a line originated from the center.
+fn draw_hand(matrix: Matrix, hand: Hand) -> Matrix {
+    let degree = hand.degree;
     let midpoint_x = matrix.midpoint_x;
     let midpoint_y = matrix.midpoint_y;
     let radian = degree * (2.0 * PI) / 360.0;
     let radius = matrix.circle_radius;
+    let aspect_ratio = matrix.aspect_ratio;
     draw_using_equation(matrix, |x, y| {
-        let error_margin = 0.00001_f32;
+        let error_margin = 0.001_f32;
 
         // Check if the points fall outside of the circle
-        if distance(
-            Point {
-                x: midpoint_x,
-                y: midpoint_y,
-            },
-            Point { x, y },
-        ) > radius * line.length
-        {
+        // by using the circle equation: (x - midpoint_x)^2 + (y - midpoint_y)^2 = radius^2
+        let left = ((x as f32) - midpoint_x).powf(2.0) / aspect_ratio
+            + ((y as f32) - midpoint_y).powf(2.0);
+        let right = ( radius * hand.length ).powf(2.0);
+        if left > right {
             false
         } else if (degree - 90.0).abs() < error_margin {
             // need to specially handle 90.0 and 180.0, as tan(90degree) and tan(180degree) is
             // infinity.
-            x >= midpoint_x && (y - midpoint_y).abs() < error_margin
+            x >= midpoint_x && (y - midpoint_y).abs() <= hand.width
         } else if (degree - 180.0).abs() < error_margin {
-            x <= midpoint_x && (y - midpoint_y).abs() < error_margin
-        } else if ((0.0..=90.0).contains(&degree) && !(x >= midpoint_x && y >= midpoint_y))
+            x <= midpoint_x && (y - midpoint_y).abs() <= hand.width
+        }
+        // Check if the points fall outside of the desired quadrant
+        else if ((0.0..=90.0).contains(&degree) && !(x >= midpoint_x && y >= midpoint_y))
             || (90.0..=180.0).contains(&degree) && !(x >= midpoint_x && y <= midpoint_y)
             || (180.0..=270.0).contains(&degree) && !(x <= midpoint_x && y <= midpoint_y)
             || (270.0..=360.0).contains(&degree) && !(x <= midpoint_x && y >= midpoint_y)
         {
             false
         } else {
+            // TODO: need to factor in the aspect_ratio when calculating the gradian
+            // The formula use here is: `(x - midpoint_x) = (tan s) * (y + midpoint_y)`
+            // where `s` is in terms of radian.
             let diff = (x - midpoint_x) - radian.tan() * (y - midpoint_y);
-            diff.abs() < line.width
+            diff.abs() < hand.width
         }
     })
 }
