1d15d03b6e8408db80291c8c6a308c444fa7df21
Wong Jia Hau <hou32hou@gmail.com>
2021-12-12 17:23:17 +0800

    feat(clock): dynamic horizontal stretching based on user input - pressing '-' thin-izes the clock - pressing '+' or '=' widens the clock - pressing 'q' quits the clock

--- src/clock.rs
+++ src/clock.rs
@@ -1,17 +1,20 @@
 use super::theme::Theme;
 use bresenham::Bresenham;
 use chrono::{DateTime, Local, Timelike};
-use colored::{self, Colorize};
 use colors_transform::Color;
 use colors_transform::Rgb;
+use crossterm::{
+    cursor,
+    event::{poll, read, Event, KeyCode},
+    style::{self, Stylize},
+    terminal, ExecutableCommand, QueueableCommand, Result,
+};
+use image::{imageops::resize, ImageBuffer, Rgb as RgbPixel};
 use line_drawing::BresenhamCircle;
 use std::f32::consts::PI;
+use std::io::{stdout, Write};
+use std::process;
 use std::time::Duration;
-
-// h/w, where:
-//   h is the actual height of each character in the terminal,
-//   w is the actual width of each character in terminal
-const ASPECT_RATIO: usize = 2;
 
 pub struct RunClockOptions {
     pub theme: Theme,
@@ -24,112 +27,150 @@ pub struct RunClockOptions {
     pub show_minute_labels: bool,
 }
 
-pub fn run_clock(options: RunClockOptions) -> Result<(), String> {
+fn new_error(message: String) -> std::io::Error {
+    std::io::Error::new(std::io::ErrorKind::Other, message.as_str())
+}
+
+struct UiState {
+    /// Aspect ratio  = character_height / character_width
+    ///
+    /// where:
+    ///
+    ///   character_height is the actual height of each character in the terminal,
+    ///   character_width is the actual width of each character in terminal
+    ///
+    /// This is needed to circularize the clock, otherwise it will look like an ellipse.
+    aspect_ratio: f32,
+}
+
+pub fn run_clock(options: RunClockOptions) -> Result<()> {
+    terminal::enable_raw_mode()?;
+
+    stdout().execute(cursor::Hide)?;
+
+    let mut state = UiState { aspect_ratio: 2.0 };
+
     loop {
-        let (width, height) =
-            term_size::dimensions().ok_or_else(|| "Unable to get term size :(".to_string())?;
-
-        let width = width / ASPECT_RATIO; // Divide by two because each pixel height is approximately double the pixel width
-
-        let midpoint_x = (width as f32) / 2.0;
-        let midpoint_y = (height as f32) / 2.0;
-        let radius = midpoint_x.min(midpoint_y) / 1.1;
-
-        let matrix = {
-            Matrix {
-                cells: vec![vec![None; width]; height],
-                height,
-                midpoint_x: width as f32 / 2.0,
-                midpoint_y: height as f32 / 2.0,
-                circle_radius: radius,
+        // Read for user input in a non-blocking manner
+        // Refer https://docs.rs/crossterm/latest/crossterm/event/index.html#examples
+        if poll(options.tick_interval)? {
+            match read()? {
+                Event::Key(event) => {
+                    if event.code == KeyCode::Char('+') || event.code == KeyCode::Char('=') {
+                        state.aspect_ratio += 0.1;
+                    } else if event.code == KeyCode::Char('-') {
+                        state.aspect_ratio -= 0.1;
+                    } else if event.code == KeyCode::Char('q') {
+                        stdout().execute(cursor::Show)?;
+                        process::exit(0)
+                    }
+                }
+                Event::Mouse(event) => println!("{:?}", event),
+                Event::Resize(width, height) => println!("New size {}x{}", width, height),
             }
-        };
-        let datetime: DateTime<Local> = Local::now();
+        }
+        draw_clock(&state, &options)?
+    }
+}
 
-        let matrix = matrix.draw_circle(Rgb::from_hex_str(options.theme.clock_face).unwrap());
+fn draw_clock(state: &UiState, options: &RunClockOptions) -> Result<()> {
+    let (screen_width, height) = term_size::dimensions()
+        .ok_or_else(|| new_error("Unable to get term size :(".to_string()))?;
+    let clock_width = (screen_width as f32) / state.aspect_ratio;
+    let midpoint_x = (clock_width as f32) / 2.0;
+    let midpoint_y = (height as f32) / 2.0;
+    let matrix = Matrix {
+        cells: vec![vec![None; clock_width as usize]; height],
+        width: clock_width as usize,
+        height,
+        midpoint_x,
+        midpoint_y,
+        circle_radius: midpoint_x.min(midpoint_y) / 1.1,
+    };
+    let datetime: DateTime<Local> = Local::now();
 
-        // Draw clock face: hour labels
-        let matrix = if options.show_hour_labels {
-            (0..12).into_iter().fold(matrix, |matrix, n| {
-                matrix.draw_hand(Hand {
-                    degree: (n as f32) / 12.0 * 360.0,
-                    thickness: HandThickness::Thin,
-                    length: 0.15,
-                    line_start: HandLineStart::FromCircumference,
-                    color: Rgb::from_hex_str(options.theme.clock_face).unwrap(),
-                })
+    let matrix = matrix.draw_circle(Rgb::from_hex_str(options.theme.clock_face).unwrap());
+
+    // Draw clock face: hour labels
+    let matrix = if options.show_hour_labels {
+        (0..12).into_iter().fold(matrix, |matrix, n| {
+            matrix.draw_hand(Hand {
+                degree: (n as f32) / 12.0 * 360.0,
+                thickness: HandThickness::Thin,
+                length: 0.15,
+                line_start: HandLineStart::FromCircumference,
+                color: Rgb::from_hex_str(options.theme.clock_face).unwrap(),
             })
-        } else {
-            matrix
-        };
+        })
+    } else {
+        matrix
+    };
 
-        // Draw clock face: minute/seconds labels
-        let matrix = if options.show_minute_labels {
-            (0..60).into_iter().fold(matrix, |matrix, n| {
-                matrix.draw_hand(Hand {
-                    degree: (n as f32) / 60.0 * 360.0,
-                    thickness: HandThickness::Thin,
-                    length: 0.05,
-                    line_start: HandLineStart::FromCircumference,
-                    color: Rgb::from_hex_str("#4C566A").unwrap(),
-                })
+    // Draw clock face: minute/seconds labels
+    let matrix = if options.show_minute_labels {
+        (0..60).into_iter().fold(matrix, |matrix, n| {
+            matrix.draw_hand(Hand {
+                degree: (n as f32) / 60.0 * 360.0,
+                thickness: HandThickness::Thin,
+                length: 0.05,
+                line_start: HandLineStart::FromCircumference,
+                color: Rgb::from_hex_str("#4C566A").unwrap(),
             })
-        } else {
-            matrix
-        };
+        })
+    } else {
+        matrix
+    };
 
-        let millisecond = datetime.timestamp_millis() % 1000;
-        let second = datetime.second() as f32;
-        let minute = datetime.minute() as f32;
-        let hour = (datetime.hour() % 12) as f32;
+    let millisecond = datetime.timestamp_millis() % 1000;
+    let second = datetime.second() as f32;
+    let minute = datetime.minute() as f32;
+    let hour = (datetime.hour() % 12) as f32;
 
-        let second = if options.tick_interval.as_millis() < 1000 {
-            second + (millisecond as f32) / 1000.0
-        } else {
-            second
-        };
-        let degree_second = second / 60.0 * 360.0;
-        let degree_minute = (minute + second / 60.0) / 60.0 * 360.0;
-        let degree_hour = (hour + minute / 60.0) / 12.0 * 360.0;
+    let second = if options.tick_interval.as_millis() < 1000 {
+        second + (millisecond as f32) / 1000.0
+    } else {
+        second
+    };
+    let degree_second = second / 60.0 * 360.0;
+    let degree_minute = (minute + second / 60.0) / 60.0 * 360.0;
+    let degree_hour = (hour + minute / 60.0) / 12.0 * 360.0;
 
-        // Firstly, draw minute hand
-        let matrix = matrix.draw_hand(Hand {
-            degree: degree_minute,
-            thickness: HandThickness::Bold,
+    // Firstly, draw minute hand
+    let matrix = matrix.draw_hand(Hand {
+        degree: degree_minute,
+        thickness: HandThickness::Bold,
+        length: 0.9,
+        line_start: HandLineStart::FromCenter,
+        color: Rgb::from_hex_str(options.theme.minute).unwrap(),
+    });
+
+    // Secondly, draw hour hand, as hour hand must be on top of minute hand
+    let matrix = matrix.draw_hand(Hand {
+        degree: degree_hour,
+        thickness: HandThickness::Bold,
+        length: 0.5,
+        line_start: HandLineStart::FromCenter,
+        color: Rgb::from_hex_str(options.theme.hour).unwrap(),
+    });
+
+    // Thirdly, draw second hand, which should be on top of hour hand & minute hand
+    let matrix = if options.show_second_hand {
+        matrix.draw_hand(Hand {
+            degree: degree_second,
+            thickness: HandThickness::Thin,
             length: 0.9,
             line_start: HandLineStart::FromCenter,
-            color: Rgb::from_hex_str(options.theme.minute).unwrap(),
-        });
+            color: Rgb::from_hex_str(options.theme.second).unwrap(),
+        })
+    } else {
+        matrix
+    };
 
-        // Secondly, draw hour hand, as hour hand must be on top of minute hand
-        let matrix = matrix.draw_hand(Hand {
-            degree: degree_hour,
-            thickness: HandThickness::Bold,
-            length: 0.5,
-            line_start: HandLineStart::FromCenter,
-            color: Rgb::from_hex_str(options.theme.hour).unwrap(),
-        });
+    // After computing the final matrix, we have to resize it
+    let matrix = matrix.rescale(screen_width);
 
-        // Thirdly, draw second hand, which should be on top of hour hand & minute hand
-        let matrix = if options.show_second_hand {
-            matrix.draw_hand(Hand {
-                degree: degree_second,
-                thickness: HandThickness::Thin,
-                length: 0.9,
-                line_start: HandLineStart::FromCenter,
-                color: Rgb::from_hex_str(options.theme.second).unwrap(),
-            })
-        } else {
-            matrix
-        };
-
-        matrix.print();
-
-        std::thread::sleep(options.tick_interval);
-
-        // Clear the screen
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    }
+    matrix.print()?;
+    Ok(())
 }
 
 #[derive(Clone, Debug)]
@@ -139,6 +180,7 @@ struct Cell {
 
 struct Matrix {
     cells: Vec<Vec<Option<Cell>>>,
+    width: usize,
     height: usize,
     midpoint_x: f32,
     midpoint_y: f32,
@@ -228,25 +270,51 @@ impl Matrix {
             })
     }
 
-    fn print(self) {
-        for row in self.cells {
-            for cell in row {
-                let character = match cell {
-                    Some(cell) => "█"
-                        .truecolor(
-                            cell.color.get_red() as u8,
-                            cell.color.get_green() as u8,
-                            cell.color.get_blue() as u8,
-                        )
-                        .to_string(),
-                    None => " ".to_string(),
-                };
-                for _ in 0..ASPECT_RATIO {
-                    print!("{}", character)
-                }
-            }
-            println!()
+    /// Apply vertical/horizontal scaling to the given matrix,
+    /// such that the clock will look like a circle instead of an ellipse.
+    /// This is because each "pixel" (or character) on a terminal is not square-ish, but a
+    /// vertical rectangle instead.
+    fn rescale(self, screen_width: usize) -> Matrix {
+        let img = matrix_to_luma_image_buffer(&self);
+        let img = resize(
+            &img,
+            screen_width as u32,
+            self.height as u32,
+            image::imageops::FilterType::Nearest,
+        );
+        Matrix {
+            cells: luma_image_buffer_to_matrix(img),
+            ..self
         }
+    }
+
+    fn print(self) -> Result<()> {
+        let mut stdout = stdout();
+
+        stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+        for (y, row) in self.cells.into_iter().enumerate() {
+            for (x, cell) in row.into_iter().enumerate() {
+                let x = x as u16;
+                let y = y as u16;
+                let character = match cell {
+                    Some(cell) => "█".with(style::Color::Rgb {
+                        r: cell.color.get_red() as u8,
+                        g: cell.color.get_green() as u8,
+                        b: cell.color.get_blue() as u8,
+                    }),
+                    None => " ".stylize(),
+                };
+
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::PrintStyledContent(character))?;
+            }
+        }
+
+        stdout.flush()?;
+
+        Ok(())
     }
 
     fn draw_using_points(mut self, points: Vec<Point>) -> Matrix {
@@ -257,6 +325,37 @@ impl Matrix {
     }
 }
 
+fn matrix_to_luma_image_buffer(matrix: &Matrix) -> ImageBuffer<RgbPixel<u8>, Vec<u8>> {
+    ImageBuffer::from_fn(
+        matrix.width as u32,
+        matrix.height as u32,
+        |x, y| match &matrix.cells[y as usize][x as usize] {
+            Some(cell) => RgbPixel([
+                cell.color.get_red() as u8,
+                cell.color.get_green() as u8,
+                cell.color.get_blue() as u8,
+            ]),
+            None => RgbPixel([255, 255, 255]),
+        },
+    )
+}
+
+fn luma_image_buffer_to_matrix(img: ImageBuffer<RgbPixel<u8>, Vec<u8>>) -> Vec<Vec<Option<Cell>>> {
+    let width = img.width() as usize;
+    let mut cells = vec![vec![None; width]; img.height() as usize];
+    img.pixels().enumerate().for_each(|(index, pixel)| {
+        let y = (index as f32 / width as f32).floor() as usize;
+        let x = index % width;
+        cells[y][x] = if pixel != &RgbPixel([255, 255, 255]) {
+            Some(Cell {
+                color: Rgb::from(pixel.0[0] as f32, pixel.0[1] as f32, pixel.0[2] as f32),
+            })
+        } else {
+            None
+        }
+    });
+    cells
+}
 struct Point {
     x: isize,
     y: isize,
