6728be0ad62415377a1237ee3a7632bb99de916b
Wong Jia Hau <hou32hou@gmail.com>
2021-12-14 16:46:19 +0800

    feat(theme): add gruvbox, monokai and lime

--- src/theme.rs
+++ src/theme.rs
@@ -7,7 +7,7 @@ pub struct Theme {
     pub clock_face: &'static str,
 }
 
-pub const THEMES: [Theme; 2] = [
+pub const THEMES: [Theme; 7] = [
     // Nord themes, https://www.nordtheme.com/
     Theme {
         name: "nord-frost",
@@ -22,5 +22,43 @@ pub const THEMES: [Theme; 2] = [
         minute: "#D08770",
         second: "#EBCB8B",
         clock_face: "#B48EAD",
+    },
+    // Gruvbox. https://github.com/morhetz/gruvbox
+    Theme {
+        name: "gruvbox-light",
+        hour: "#928374",
+        minute: "#a89984",
+        second: "#bdae93",
+        clock_face: "#d5c4a1",
+    },
+    Theme {
+        name: "gruvbox-dark",
+        hour: "#d5c4a1",
+        minute: "#bdae93",
+        second: "#a89984",
+        clock_face: "#928374",
+    },
+    // Monokai. https://gist.github.com/r-malon/8fc669332215c8028697a0bbfbfbb32a
+    Theme {
+        name: "monokai",
+        hour: "#66d9ef",
+        minute: "#ae81ff",
+        second: "#f92672",
+        clock_face: "#a6e22e",
+    },
+    // Lime. https://encycolorpedia.com/b3cd4f#:~:text=The%20hexadecimal%20color%20code%20%23b3cd4f,%25%20saturation%20and%2056%25%20lightness.
+    Theme {
+        name: "lime-light",
+        hour: "#8da729",
+        minute: "#99b436",
+        second: "#a6c043",
+        clock_face: "#b3cd4f",
+    },
+    Theme {
+        name: "lime-dark",
+        hour: "#dbf474",
+        minute: "#cde768",
+        second: "#c0da5b",
+        clock_face: "#b3cd4f",
     },
 ];
